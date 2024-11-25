d8405b902b5e753ba5bac397437de4c7431bdf03
lolay92 <laleyecheryf@gmail.com>
2024-11-25 15:47:25 +0100

    Source: Shared pointer structure implementation

--- src/include/lib.h
+++ src/include/lib.h
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdatomic.h>
+#include <errno.h>
 
-typedef void (*destructor_fn)(void*);
+typedef void (*res_destructor_fn)(void* resource);
 
-typedef struct Control {
-	void* data;
-	destructor_fn destructor;
-	size_t ref_count;
-	size_t data_size;
-} control_t;
+typedef struct Controller {
+	void* resource;
+	res_destructor_fn destructor;
+	atomic_size_t ref_count;
+	size_t resource_size;
+} controller_t;
 
 typedef struct SharedPtr {
-	control_t* controller;
+	controller_t* controller;
 } shared_ptr_t;
 
-typedef spinlock_t; 
- 
-control_t* create_controller(void* data, size_t data_size, destructor_fn destructor);
-shared_ptr_t* init_shared_ptr();
-shared_ptr_t* create_shared_ptr(void* data, size_t data_size, destructor_fn destructor);
-void safe_increment_ref_count(spinlock_t* spinlock, size_t ref_count);
+void fail(const char* message); 
+controller_t* create_controller(size_t resource_size, res_destructor_fn destructor);
+shared_ptr_t* init_shared_ptr(size_t resource_size, res_destructor_fn destructor);
+shared_ptr_t* make_shared(size_t resource_size, res_destructor_fn destructor);
 shared_ptr_t* copy_shared_ptr(shared_ptr_t* src);
 shared_ptr_t* move_shared_ptr(shared_ptr_t* src);
-shared_ptr_t* make_shared(void* data, size_t size);
 void destroy_shared_ptr(shared_ptr_t*);
+
--- src/lib.c
+++ src/lib.c
@@ -1,62 +1,62 @@
 #include "include/lib.h"
 
+void fail(const char* message) {
+    if (errno) {
+        perror(message); 
+    } else {
+        printf("ERROR: ‰s\n", message); 
+    }
+    exit(1); 
+}
 
-control_t* create_controller(void* data, size_t data_size, destructor_fn destructor){
-    control_t* controller = malloc(sizeof(control_t));
-    if(!controller){
-        perror("Failed to allocate memory for controller creation!");
-        return NULL;
-    };
+controller_t* create_controller(size_t resource_size, res_destructor_fn destructor){
+    controller_t* controller = malloc(sizeof(controller_t));
+    if(!controller){ fail("Failed to allocate memory for controller creation!"); }
 
-    controller->data = data;
+    controller->resource = malloc(resource_size);
+    if(!controller->resource){ fail("Failed to allocate memory for resource initialization!"); }
+
     controller->destructor = destructor;
-    controller->data_size = data_size;
+    controller->resource_size = resource_size;
     controller->ref_count= 1;
 
     return controller;
 }
 
-shared_ptr_t* init_shared_ptr(){
-    control_t* controller = create_controller(NULL, 0, NULL);
+shared_ptr_t* init_shared_ptr(size_t resource_size, res_destructor_fn destructor){
+    controller_t* controller = create_controller(resource_size, destructor);
     shared_ptr_t* sp = malloc(sizeof(shared_ptr_t));
-    if(!sp){
-        perror("Failed to allocate memory for shared_ptr initialization!");
-        return NULL;
-    };
+    if(!sp){ fail("Failed to allocate memory for shared_ptr initialization!"); }
     sp->controller = controller;
 
     return sp;
 }
 
-shared_ptr_t* create_shared_ptr(void* data, size_t data_size, destructor_fn destructor){
-    control_t* controller = create_controller(data, data_size, destructor);
+shared_ptr_t* make_shared(size_t resource_size, res_destructor_fn destructor){
+    controller_t* controller = malloc(sizeof(controller_t) + resource_size); 
+    if(!controller){ fail("Failed to allocate memory for controller + shared_ptr creation!"); }
+
+    controller->resource = controller + sizeof(controller_t); 
+    atomic_fetch_add(&(controller->ref_count), 1);
+    controller->destructor = destructor; 
+    controller->resource_size = resource_size; 
+
     shared_ptr_t* sp = malloc(sizeof(shared_ptr_t));
-    if(!sp){
-        perror("Failed to allocate memory for shared_ptr initialization!");
-        return NULL;
-    };
-    sp->controller = controller;
+    if(!sp){ fail("Failed to allocate memory for shared_ptr creation!"); }
 
-    return sp;
-}
+    sp->controller = controller; 
 
-// TO IMPLEMENT
-void safe_increment_ref_count(spinlock_t* spinlock, size_t ref_count){
-
+    return sp; 
 }
 
 shared_ptr_t* copy_shared_ptr(shared_ptr_t* src){
     shared_ptr_t* copy = malloc(sizeof(shared_ptr_t));
-    if(!copy){
-        perror("Failed to allocate memory for shared_ptr initialization!");
-        return NULL;
-    };
-
+    if(!copy){ fail("Failed to allocate memory for shared_ptr initialization!"); }
+    
     if(src->controller){
         copy->controller = src->controller;
-        // safe_increment_ref_count(spinlock, src->controller->ref_count)
-        // src->controller->ref_count++; // Later implement spinlock for thread safety
-    }; 
+        atomic_fetch_add(&(src->controller->ref_count), 1);
+    };
 
     return copy; 
 }
@@ -66,26 +66,31 @@ shared_ptr_t* move_shared_ptr(shared_ptr_t* src){
     if(!dest){
         perror("Failed to allocate memory for shared_ptr initialization!");
         return NULL;
-    };
+    }
 
     if(src->controller){
         dest->controller = src->controller;
-        // safe_increment_ref_count(spinlock, src->controller->ref_count)
-        // src->controller->ref_count++; // Later implement spinlock for thread safety
-    };
+        atomic_fetch_add(&(src->controller->ref_count), 1);
+    }
 
     destroy_shared_ptr(src);
-    free(src); 
-
     return dest; 
 }
 
-shared_ptr_t* make_shared(void* data, size_t data_size){
+void destroy_shared_ptr(shared_ptr_t* sp){
+    if(!sp || !(sp->controller) || !(sp->controller->resource)) {
+        perror("Unsafe memory access during shared pointer destruction!");
+        return; 
+    }
+    atomic_fetch_sub(&sp->controller->ref_count, 1);
     
+    if(atomic_load(&(sp->controller->ref_count)) == 0) { 
+        if(sp->controller->destructor) {
+            sp->controller->destructor(sp->controller->resource);
+        } 
+        free(sp->controller);
+    } else {
+        sp->controller = NULL; 
+    }
+    free(sp);
 }
-
-void destroy_shared_ptr(shared_ptr_t*){
-
-}
-
-
